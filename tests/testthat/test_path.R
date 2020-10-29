b135377533204542a158bf08aa6723521cdef199
Jeff-King <szwjf08@gmail.com>
2020-10-29 12:35:59 +0800

    add unit test for the path in the package

---
+++ tests/testthat/test_path.R
@@ -0,0 +1,20 @@
+context("test the path in the package")
+
+test_that("Package library", {
+    libPath <- capture.output(SharedObject:::pkgconfig("PKG_LIBS"))
+    expect_true(file.exists(libPath))
+})
+
+test_that("Package flags", {
+    libFlags <- capture.output(SharedObject:::pkgconfig("PKG_CPPFLAGS"))
+    expect_true(length(libFlags)==0)
+})
+
+test_that("shared memory path", {
+    memoryPath <- getSharedMemoryPath()
+    if(getOS()=="linux"){
+        expect_true(dir.exists(memoryPath))
+    }else{
+        expect_true(memoryPath=="")
+    }
+})
